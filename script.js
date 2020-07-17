4dec2f9e6418614bef9bd97f33e38ab3be20d9db
David <davidagasiev2005@gmail.com>
2020-07-17 18:10:51 +0400

    menu btn

---
+++ script.js
@@ -0,0 +1,12 @@
+//Menu Button
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
